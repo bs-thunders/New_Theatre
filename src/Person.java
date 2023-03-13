1684108b2230d0a5f013c1b9bab4dc4aa72939b7
Bhathiya <120437641+bs-thunders@users.noreply.github.com>
2023-03-13 23:20:40 +0530

    Person and Ticket
    Add new two classes name Person and Tickets

---
+++ src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    //Task (9)
+
+    //get name ,surname and email
+}
---
+++ src/Ticket.java
@@ -0,0 +1,10 @@
+class Ticket {
+    //Task (10)
+    public static void main(String[] args) {
+        //constructor row,sear,price and Person
+    }
+    private static void print(){
+        //prints all the information from a ticket: Person name, Person surname, Person email, row, seat, and price.
+    }
+
+}
